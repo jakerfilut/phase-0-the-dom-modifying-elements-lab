ad913ab2bbea877842bb8ab5b686bbab17da4554
Jake <jakerfilut@gmail.com>
2021-11-05 10:53:02 -0600

    commit tree a16bde73c051c75bc54df5118295bde24d66006b

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+main.remove()
+
+const  newHeader = document.createElement('h1');
+newHeader.idName = "victory";
+newHeader.setAttribute('id', "victory");
+const p = document.createElement('p');
+p.innerHTML = "Jake is the champion";
+newHeader.appendChild(p);
+
